143fa78bba50fe882a376fdf2761c037b4c50753
Khushi Kunwar <kunwarkhushi75@gmail.com>
2026-08-13 22:01:45 +0530

    Time: 400 ms (31.11%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,6 +1,14 @@
-# Write your MySQL query statement below
+-- # Write your MySQL query statement below
+-- select e1.name
+-- from employee e1, employee e2
+-- where e1.id = e2.managerId 
+-- group by e1.id , e1.name
+-- having count(*)>=5
+
+# Write your MySQL query statement below 
 select e1.name
-from employee e1, employee e2
-where e1.id = e2.managerId 
-group by e1.id , e1.name
-having count(*)>=5
+from Employee as e1
+join Employee as e2 on 
+e1.id = e2.managerId
+group by e2.managerId
+having count(e2.managerId)>=5;
