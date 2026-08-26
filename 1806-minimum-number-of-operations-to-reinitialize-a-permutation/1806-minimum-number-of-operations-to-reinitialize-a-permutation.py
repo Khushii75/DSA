8991ba2c2248ebad5fbbd3e7d2776ea29aa621ac
Khushi Kunwar <kunwarkhushi75@gmail.com>
2026-08-26 09:32:48 +0530

    Time: 2 ms (57.27%), Space: 19.3 MB (60.68%) - LeetHub

---
+++ 1806-minimum-number-of-operations-to-reinitialize-a-permutation/1806-minimum-number-of-operations-to-reinitialize-a-permutation.py
@@ -0,0 +1,11 @@
+class Solution:
+    def reinitializePermutation(self, n: int) -> int:
+        res, i=0,1
+        while res ==0 or i>1:
+            if i<n/2:
+                i *= 2
+            else:
+                i= (i-n/2)*2+1
+            res += 1
+        return res
+        
